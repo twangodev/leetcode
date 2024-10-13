7f20db2fae46eb42c9426628a7dc7a7092321837
James Ding <jamesding365@gmail.com>
2024-10-12 23:26:02 -0500

    Add LongestSubstringWithoutRepeatingCharacters.kt

--- src/main/kotlin/LongestSubstringWithoutRepeatingCharacters.kt
+++ src/main/kotlin/LongestSubstringWithoutRepeatingCharacters.kt
@@ -1,4 +1,19 @@
+import kotlin.math.max
+
 fun lengthOfLongestSubstring(s: String): Int {
+    var length = 0
+    var left = 0
+    val map = mutableMapOf<Char, Int>()
 
+    for (right in s.indices) {
+        val charRight = s[right]
+        if (map.containsKey(charRight)) {
+            val i = map[charRight]!! + 1
+            if (i > left) left = i
+        }
+        map[charRight] = right
+        length = max(right + 1 - left, length)
+    }
+
+    return length
 }
-
