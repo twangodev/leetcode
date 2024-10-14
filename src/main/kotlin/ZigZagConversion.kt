4f6102b27274ca1ced3f380c94f187de4aa4855c
James Ding <jamesding365@gmail.com>
2024-10-14 16:02:52 -0500

    Add ZigZagConversion.kt

--- src/main/kotlin/ZigZagConversion.kt
+++ src/main/kotlin/ZigZagConversion.kt
@@ -1,18 +1,35 @@
 fun convert(s: String, numRows: Int): String {
+    if (numRows == 1) return s
+
     val arrays = Array(numRows) { CharArray(s.length) }
 
     var column = 0
-    var row = 0
-    var carry = 0
+    var row = -1
+    var reversing = false
 
     for (c in s) {
         if (row >= numRows) {
-            row = 0
-            column++
+            reversing = true
+            row--
         }
-
-        arrays[row][column] = c
-        row++
+        if (row <= 0) {
+            reversing = false
+            row++
+        }
+        if (!reversing) {
+            arrays[row++][column] = c
+        } else {
+            arrays[--row][++column] = c
+        }
     }
 
+    val sb = StringBuilder()
+    for (ca in arrays) {
+        for (c in ca) {
+            if (c == '\u0000') continue
+            sb.append(c)
+        }
+    }
+
+    return sb.toString()
 }
